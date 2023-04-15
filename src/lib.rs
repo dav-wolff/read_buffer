6b3d2859096295bc8996e39fcc7a3bee074590fb
David Wolff <david@dav.dev>
2023-04-15 17:30:39 +0200

    Add ReadBuffer::read_from method

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-#[derive(Debug, Eq, PartialEq)]
+use std::io;
+use std::io::Read;
+
+#[derive(Debug)]
 pub struct ReadBuffer<const SIZE: usize> {
 	buffer: [u8; SIZE],
 }
@@ -9,6 +12,11 @@ impl<const SIZE: usize> ReadBuffer<SIZE> {
 			buffer: [0u8; SIZE],
 		}
 	}
+	
+	pub fn read_from(&mut self, source: &mut impl Read) -> Result<&[u8], io::Error> {
+		let length = source.read(&mut self.buffer)?;
+		Ok(&self.buffer[..length])
+	}
 }
 
 #[cfg(test)]
@@ -18,13 +26,50 @@ mod tests {
 	#[test]
 	fn empty_buffer() {
 		let buffer = ReadBuffer::new();
-		assert_eq!(buffer, ReadBuffer {
-			buffer: [0u8; 16],
-		});
+		assert_eq!(
+			buffer.buffer,
+			[0u8; 256]
+		);
 		
-		let buffer: ReadBuffer<8> = ReadBuffer::new();
-		assert_eq!(buffer, ReadBuffer {
-			buffer: [0u8; 8],
-		});
+		let buffer: ReadBuffer<128> = ReadBuffer::new();
+		assert_eq!(
+			buffer.buffer,
+			[0u8; 128]
+		);
+	}
+	
+	#[test]
+	fn read() {
+		let mut buffer: ReadBuffer<256> = ReadBuffer::new();
+		let data = [1, 1, 2, 3, 5, 8, 13, 21];
+		let mut data = &data[..];
+		
+		let result = buffer.read_from(&mut data).unwrap();
+		assert_eq!(result.len(), 8);
+		assert_eq!(
+			result,
+			[1, 1, 2, 3, 5, 8, 13, 21]
+		);
+	}
+	
+	#[test]
+	fn read_partial() {
+		let mut buffer: ReadBuffer<8> = ReadBuffer::new();
+		let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+		let mut data = &data[..];
+		
+		let result = buffer.read_from(&mut data).unwrap();
+		assert_eq!(result.len(), 8);
+		assert_eq!(
+			result,
+			[1, 2, 3, 4, 5, 6, 7, 8]
+		);
+		
+		let result = buffer.read_from(&mut data).unwrap();
+		assert_eq!(result.len(), 2);
+		assert_eq!(
+			result,
+			[9, 10]
+		);
 	}
 }
